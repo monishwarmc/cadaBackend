b48c1187c3df181e5851480c62ebe34f77bea2df
monishwarmc <monishwar69@gmail.com>
2025-02-26 13:31:42 -0500

    * domain

--- src/main/java/com/cada/backend/BackendCaDaApplication.java
+++ src/main/java/com/cada/backend/BackendCaDaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin
+@CrossOrigin(origins = "*")
 public class BackendCaDaApplication {
 
 	public static void main(String[] args) {
